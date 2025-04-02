137a22997593441074cecd20a464fb196f58cb75
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-04-02 18:52:18 +0200

    Add EventListener to the numbers and operators buttons to populate the display. Add EventListener to the Clear button to clear the display when clicked.

--- scripts.js
+++ scripts.js
@@ -1,39 +1,49 @@
 function add(a, b) {
-	return a + b;
+    return a + b;
 }
 
 function subtract(a, b) {
-	return a - b;
+    return a - b;
 }
 
 function multiply(a, b) {
-	return a * b;
+    return a * b;
 }
 
 function divide(a, b) {
-	return a / b;
+    return a / b;
 }
 
 function operate(number1, number2, operator) {
-	switch (operator) {
-		case "+":
-			return add(number1, number2);
-			break;
-			
-		case "-":
-			return subtract(number1, number2);
-			break;
-		
-		case "*":
-			return multiply(number1, number2);
-			break;
-			
-		case "/":
-			return divide(number1, number2);
-			break;
-	}
+    switch (operator) {
+        case "+":
+            return add(number1, number2);
+            break;
+
+        case "-":
+            return subtract(number1, number2);
+            break;
+
+        case "*":
+            return multiply(number1, number2);
+            break;
+
+        case "/":
+            return divide(number1, number2);
+            break;
+    }
 }
 
 const number1 = 0;
 const number2 = 0;
 const operator = "";
+
+const display = document.querySelector("#display");
+const digits = document.querySelectorAll("#panel .digits");
+const clear = document.querySelector("#clear");
+
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => { display.textContent = display.textContent + digit.textContent })
+});
+
+clear.addEventListener("click", () => { display.textContent = "" });
