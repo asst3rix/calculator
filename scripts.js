83b489ffbe00b05d1f408f98b4a3095a204e174c
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-04-02 17:13:25 +0200

    Create the function operate() that takes 2 number and one operator.

--- scripts.js
+++ scripts.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
 	return a / b;
 }
+
+function operate(number1, number2, operator) {
+	switch (operator) {
+		case "+":
+			return add(number1, number2);
+			break;
+			
+		case "-":
+			return subtract(number1, number2);
+			break;
+		
+		case "*":
+			return multiply(number1, number2);
+			break;
+			
+		case "/":
+			return divide(number1, number2);
+			break;
+	}
+}
+
+const number1 = 0;
+const number2 = 0;
+const operator = "";
