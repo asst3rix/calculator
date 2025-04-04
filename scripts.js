2a8808e59f6f95596a0c173de75c3323a8d36ab5
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-04-04 17:47:11 +0200

    Improve the script so it can do more than one operation before clicking on egual.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,72 @@
+/*** VARIABLES ***/
+
+let firstNumber = false;
+let secondNumber = false;
+let operator = "";
+let testOperator = false;
+
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const clear = document.querySelector("#clear");
+const egual = document.querySelector("#egual");
+const minus = document.querySelector("#minus");
+const percentage = document.querySelector("#percentage");
+
+/*** EVENT LISTENERS ***/
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if (display.textContent.at(0) === "0") {
+            display.textContent = display.textContent.slice(1);
+        }
+
+        if (testOperator) {
+            display.textContent = "";
+            testOperator = false;
+        }
+
+        display.textContent = display.textContent + number.textContent;
+    })
+});
+
+operators.forEach((localOperator) => {
+    localOperator.addEventListener("click", () => {
+        if (firstNumber !== false) {
+            display.textContent = operate(parseInt(firstNumber), parseInt(display.textContent), operator);   
+        }
+        firstNumber = display.textContent;
+        operator = localOperator.textContent;
+        testOperator = true;
+    })
+});
+
+egual.addEventListener("click", () => {
+    secondNumber = display.textContent;
+    display.textContent = operate(parseInt(firstNumber), parseInt(secondNumber), operator);
+});
+
+clear.addEventListener("click", () => {
+    display.textContent = "0";
+    firstNumber = false;
+    secondNumber = false;
+    operator = "";
+});
+
+minus.addEventListener("click", () => {
+    if (display.textContent.at(0) === "-") {
+        display.textContent = display.textContent.slice(1);
+    } else {
+        display.textContent = "-" + display.textContent;
+    }
+});
+
+percentage.addEventListener("click", () => {
+    display.textContent = parseInt(display.textContent) * 0.01;
+});
+
+/*** FUNCTIONS ***/
+
 function add(a, b) {
     return a + b;
 }
@@ -33,63 +102,3 @@ function operate(number1, number2, operator) {
             break;
     }
 }
-
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
-let testOperator = false;
-
-const display = document.querySelector("#display");
-const numbers = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators");
-const clear = document.querySelector("#clear");
-const egual = document.querySelector("#egual");
-const minus = document.querySelector("#minus");
-const percentage = document.querySelector("#percentage");
-
-numbers.forEach((number) => {
-    number.addEventListener("click", () => {
-        if (display.textContent.at(0) === "0") {
-            display.textContent = display.textContent.slice(1);
-        }
-
-        if (testOperator) {
-            display.textContent = "";
-            testOperator = false;
-        }
-
-        display.textContent = display.textContent + number.textContent;
-    })
-});
-
-operators.forEach((localOperator) => {
-    localOperator.addEventListener("click", () => {
-        firstNumber = display.textContent;
-        operator = localOperator.textContent;
-        testOperator = true;
-    })
-});
-
-egual.addEventListener("click", () => {
-    secondNumber = display.textContent;
-    display.textContent = operate(parseInt(firstNumber), parseInt(secondNumber), operator);
-});
-
-clear.addEventListener("click", () => {
-    display.textContent = "0";
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = "";
-});
-
-minus.addEventListener("click", () => {
-    if (display.textContent.at(0) === "-") {
-        display.textContent = display.textContent.slice(1);
-    } else {
-        display.textContent = "-" + display.textContent;
-    }
-});
-
-percentage.addEventListener("click", () => {
-    display.textContent = parseInt(display.textContent) * 0.01;
-});
