a393bcdcf5836a993225e2c0dbb725d5a881bb5a
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-04-14 18:37:54 +0200

    Complete reorganization of the script following keyboard recognition. Add functions that handle input (number, operators, etc). Add a function (handleInput) that manages all those inputs. Delete eventListener on specific buttons like numbers and operators. Add the same eventListener on all the buttons that use the handleInput function.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /*** VARIABLES ***/
 
-let firstNumber = false;
-let secondNumber = false;
+let firstNumber = null;
+let secondNumber = null;
 let operator = "";
 let operatorFlag = false;
 let equalFlag = false;
@@ -10,8 +10,8 @@ let numberFlag = false;
 const displayLimit = 8;
 
 function resetValues() {
-    firstNumber = false;
-    secondNumber = false;
+    firstNumber = null;
+    secondNumber = null;
     operator = "";
     equalFlag = false;
     commaFlag = false;
@@ -19,92 +19,18 @@ function resetValues() {
 }
 
 const display = document.querySelector("#display");
-const numbers = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators");
-const clear = document.querySelector("#clear");
-const equal = document.querySelector("#equal");
-const minus = document.querySelector("#minus");
-const percentage = document.querySelector("#percentage");
-const comma = document.querySelector("#comma");
-const backspace = document.querySelector("#backspace");
+const buttons = document.querySelectorAll("#panel button");
 
 /*** EVENT LISTENERS ***/
 
-numbers.forEach((number) => {
-    number.addEventListener("click", () => {
-        updateDisplay(number.textContent);
-        numberFlag = true;
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        handleInput(button.textContent);
     })
-});
-
-operators.forEach((localOperator) => {
-    localOperator.addEventListener("click", () => {
-        // We check that we pressed on a number.
-        // This is for the case someone press a number and then press mutiples times an operator.
-        if (numberFlag) {
-            // If firstNumber !== false, it means that we are on a multiple operation.
-            if (firstNumber !== false) {
-                display.textContent = operate(firstNumber, display.textContent, operator);
-            }
-            firstNumber = display.textContent;
-            operator = localOperator.textContent;
-            operatorFlag = true;
-            numberFlag = false;
-        }
-    })
-});
-
-equal.addEventListener("click", () => {
-    if (firstNumber !== false) {
-        secondNumber = display.textContent;
-        let result = operate(firstNumber, secondNumber, operator);
-        // To counter add(0.78,2) = 2.7800000000000002.
-        result = Math.round(result * 1e10) / 1e10;
-        display.textContent = result.toString().slice(0, displayLimit);
-    }
-    equalFlag = true;
-});
-
-clear.addEventListener("click", () => {
-    display.textContent = "0";
-    resetValues();
-});
-
-minus.addEventListener("click", () => {
-    if (display.textContent.at(0) === "-") {
-        display.textContent = display.textContent.slice(1);
-    } else {
-        display.textContent = "-" + display.textContent;
-    }
-});
-
-percentage.addEventListener("click", () => {
-    display.textContent = operate(display.textContent, "0.01", "*");
-});
-
-comma.addEventListener("click", () => {
-    if (equalFlag) {
-        display.textContent = "0.";
-        commaFlag = true;
-    }
-    else {
-        if (!commaFlag) {
-            display.textContent = display.textContent + comma.textContent;
-            commaFlag = true;
-        }
-    }
-});
-
-backspace.addEventListener("click", () => {
-    if (display.textContent.length === 1) {
-        display.textContent = "0";
-    } else {
-        display.textContent = display.textContent.slice(0, display.textContent.length - 1);
-    }
 });
 
 document.addEventListener("keydown", (key) => {
-    updateDisplay(key.key);
+    handleInput(key.key);
 });
 
 /*** FUNCTIONS ***/
@@ -122,7 +48,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b === 0) {
+    if (b === "0") {
         return "Nope.";
     } else {
         return parseFloat(a) / parseFloat(b);
@@ -149,7 +75,46 @@ function operate(number1, number2, operator) {
     }
 }
 
-function updateDisplay(value) {
+function handleInput(value) {
+    const testNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const testOperators = ["/", "*", "-", "+"];
+
+    switch (true) {
+        case testNumbers.includes(value):
+            handleNumbers(value);
+            break;
+
+        case testOperators.includes(value):
+            handleOperators(value);
+            break;
+
+        case value === ".":
+            handleDecimal(value);
+            break;
+
+        case value === "=" || value === "Enter":
+            handleEqual();
+            break;
+
+        case value === "<<" || value === "Backspace":
+            handleBackspace();
+            break;
+
+        case value === "C":
+            handleClear();
+            break;
+
+        case value === "+/-":
+            handlePlusMinus();
+            break;
+
+        case value === "%":
+            handlePercentage();
+            break;
+    }
+}
+
+function handleNumbers(value) {
     // For cases like "0758". We remove the 0 at the beginning.
     if (display.textContent.at(0) === "0" && display.textContent.at(1) !== ".") {
         display.textContent = display.textContent.slice(1);
@@ -166,9 +131,10 @@ function updateDisplay(value) {
     // If we pressed equal
     if (equalFlag) {
         // and commaFlag at true, it means that the users has pressed "." right after the equal so we display "0.".
+        // We also reset the values because it's a new operation.
         if (commaFlag) {
-            display.textContent = "0.";
             resetValues();
+            display.textContent = "0.";
             commaFlag = true;
             // Else we go with a number not starting by 0.
         } else {
@@ -180,4 +146,74 @@ function updateDisplay(value) {
     if (display.textContent.length < displayLimit) {
         display.textContent = display.textContent + value;
     }
+
+    numberFlag = true;
+}
+
+function handleOperators(value) {
+    // We check that we pressed on a number.
+    // This is for the case someone press a number and then press mutiples times an operator.
+    if (numberFlag) {
+        // If firstNumber !== false, it means that we are on a multiple operation.
+        if (firstNumber !== null) {
+            display.textContent = operate(firstNumber, display.textContent, operator);
+        }
+    }
+
+    firstNumber = display.textContent;
+    operator = value;
+    operatorFlag = true;
+    numberFlag = false;
+    equalFlag = false;
+}
+
+function handleDecimal(comma) {
+    if (equalFlag) {
+        display.textContent = "0.";
+    }
+    else {
+        if (!commaFlag) {
+            display.textContent = display.textContent + comma;
+        }
+    }
+    commaFlag = true;
+}
+
+function handleEqual() {
+    if (firstNumber !== null) {
+        secondNumber = display.textContent;
+        let result = operate(firstNumber, secondNumber, operator);
+        if (typeof result === "number") {
+            // To counter add(0.78,2) = 2.7800000000000002.
+            result = Math.round(result * 1e10) / 1e10;
+        }
+        display.textContent = result.toString().slice(0, displayLimit);
+    }
+    resetValues();
+    equalFlag = true;
+}
+
+function handleBackspace() {
+    if (display.textContent.length === 1) {
+        display.textContent = "0";
+    } else {
+        display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    }
+}
+
+function handleClear() {
+    display.textContent = "0";
+    resetValues();
+}
+
+function handlePlusMinus() {
+    if (display.textContent.at(0) === "-") {
+        display.textContent = display.textContent.slice(1);
+    } else {
+        display.textContent = "-" + display.textContent;
+    }
+}
+
+function handlePercentage() {
+    display.textContent = operate(display.textContent, "0.01", "*");
 }
